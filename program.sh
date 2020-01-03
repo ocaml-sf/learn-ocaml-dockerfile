b0a7d33b116c9e12ca17aa33b5606ad4b608fab3
Astyax NOUREL <aseretars@gmail.com>
2020-01-03 12:04:37 +0100

    added Dockerfile & script program.sh

---
+++ program.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+download_repository() {
+	echo "swift download $1"
+	swift download $1
+}
+
+upload_repository() {
+	swift upload $1 repository/ sync/
+} 
+
+trap 'kill -TERM $PID; wait $PID; upload_repository $1; exit 143' TERM
+
+download_repository $1
+dumb-init learn-ocaml --sync=sync --repo=repository &
+PID=$!
+wait $PID

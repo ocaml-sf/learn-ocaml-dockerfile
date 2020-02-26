2175e7c1d0d3b40971cc16d083e17e6e7622bc15
Astyax NOUREL <aseretars@gmail.com>
2020-02-26 20:40:52 +0100

    added a watcher of data

--- program.sh
+++ program.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# 10 min interval
+INTERVAL=600
+
 download_repository() {
 	echo "swift download $1"
 	swift download $1
@@ -9,9 +12,24 @@ upload_repository() {
 	swift upload $1 repository/ sync/
 } 
 
+CONTINUE=true
+watch_upload() {
+	while $CONTINUE
+	do
+		upload_repository $1
+		sleep $INTERVAL
+	done
+}
+
 trap 'kill -TERM $PID; wait $PID; upload_repository $1; exit 143' TERM
 
 download_repository $1
 dumb-init learn-ocaml --sync=sync --repo=repository &
-PID=$!
-wait $PID
+PID_INSTANCE=$!
+
+watch_upload &
+PID_WATCH=$!
+
+wait $PID_INSTANCE
+CONTINUE=false
+wait $PID_WATCH

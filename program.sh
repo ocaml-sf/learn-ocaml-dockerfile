753105574f65339ebfcf7baf04c764e9b9e510f9
Astyax NOUREL <aseretars@gmail.com>
2020-02-26 22:36:45 +0100

    program.sh: adapting Shell indentation

--- program.sh
+++ program.sh
@@ -5,30 +5,30 @@ INTERVAL=10
 STOP=stop
 
 download_repository() {
-	echo "swift download $1"
-	swift download $1
+    echo "swift download $1"
+    swift download $1
 }
 
 upload_repository() {
-	swift upload $1 repository/ sync/
+    swift upload $1 repository/ sync/
 } 
 
 watch_upload() {
-	while ! [ -f $STOP ]
-	do
-		sleep $INTERVAL
-		date
-		upload_repository $1
-	done
+    while ! [ -f $STOP ]
+    do
+	sleep $INTERVAL
+	date
+	upload_repository $1
+    done
 }
 
 before_exit() {
-	kill -TERM $2
-	wait $2
-	touch $STOP
-	wait $3
-	rm $STOP
-	upload_repository $1
+    kill -TERM $2
+    wait $2
+    touch $STOP
+    wait $3
+    rm $STOP
+    upload_repository $1
 }
 
 trap 'before_exit $1 $PID_INSTANCE $PID_WATCH; exit 143' TERM

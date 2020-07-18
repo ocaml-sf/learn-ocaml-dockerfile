3943c97fbc0349af76efe1f70cda0a82350ba8c6
Astyax NOUREL <aseretars@gmail.com>
2020-07-18 14:13:14 +0200

    reindenting lines

--- program.sh
+++ program.sh
@@ -3,26 +3,26 @@ REPOSITORY=repository
 SYNC=sync
 
 download_repository() {
-	echo "swift download $1"
-	swift download $1
-	unzip -q $REPOSITORY.zip
-	echo "unzipping $REPOSITORY.zip returns $?"
-	rm -f $REPOSITORY.zip
-	if [ -f $SYNC.zip ]
-	then
-		unzip -q $SYNC.zip
-		echo "unzipping $SYNC.zip returns $?"
-		rm -f $SYNC.zip
-	fi
+    echo "swift download $1"
+    swift download $1
+    unzip -q $REPOSITORY.zip
+    echo "unzipping $REPOSITORY.zip returns $?"
+    rm -f $REPOSITORY.zip
+    if [ -f $SYNC.zip ]
+    then
+        unzip -q $SYNC.zip
+        echo "unzipping $SYNC.zip returns $?"
+        rm -f $SYNC.zip
+    fi
 }
 
 upload_repository() {
-	zip -q $REPOSITORY.zip -r $REPOSITORY
-	echo "zipping $REPOSITORY returns $?"
-	zip -q $SYNC.zip -r $SYNC
-	echo "zipping $SYNC returns $?"
-	echo "swift upload $1 $REPOSITORY.zip $SYNC.zip"
-	swift upload --changed $1 $REPOSITORY.zip $SYNC.zip
+    zip -q $REPOSITORY.zip -r $REPOSITORY
+    echo "zipping $REPOSITORY returns $?"
+    zip -q $SYNC.zip -r $SYNC
+    echo "zipping $SYNC returns $?"
+    echo "swift upload $1 $REPOSITORY.zip $SYNC.zip"
+    swift upload --changed $1 $REPOSITORY.zip $SYNC.zip
 }
 
 trap 'kill -TERM $PID; wait $PID; upload_repository $1; exit 143' TERM

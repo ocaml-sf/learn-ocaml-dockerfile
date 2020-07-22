e25c8ca75e0307e0f1ff6d9240c61fa8257ffb97
Astyax NOUREL <aseretars@gmail.com>
2020-07-22 12:50:22 +0200

    fixing shellcheck warnings/issues

--- program.sh
+++ program.sh
@@ -2,9 +2,13 @@
 REPOSITORY=repository
 SYNC=sync
 
+error() {
+    echo "$1 failed" >&2
+}
+
 download_repository() {
     echo "swift download $1"
-    swift download $1
+    swift download "$1"
     unzip -q $REPOSITORY.zip
     echo "unzipping $REPOSITORY.zip returns $?"
     rm -f $REPOSITORY.zip
@@ -18,14 +22,14 @@ download_repository() {
 
 optimize_sync() {
     echo "optimizing using git gc"
-    cd $SYNC
-    DIRS=$(find -mindepth 4 -maxdepth 4 -type d -not -path "\./\.git*")
+    cd "$SYNC" || error "optimizing_sync: cd $SYNC" || return
+    DIRS=$(find . -mindepth 4 -maxdepth 4 -type d -not -path "\./\.git*")
     for i in $DIRS
     do
-        echo $i
-        cd $i
+        echo "$i"
+        cd "$i" || error "optimizing_sync: cd $i" || continue
         git gc -v
-        cd -
+        cd - || error "optimizing_sync: cd - (assert false)" || continue
     done
     git gc
     cd ..
@@ -40,12 +44,12 @@ upload_repository() {
     zip -q $SYNC.zip -r $SYNC
     echo "zipping $SYNC returns $?"
     echo "swift upload $1 $REPOSITORY.zip $SYNC.zip"
-    swift upload --changed $1 $REPOSITORY.zip $SYNC.zip
+    swift upload --changed "$1" $REPOSITORY.zip $SYNC.zip
 }
 
 trap 'kill -TERM $PID; wait $PID; upload_repository $1; exit 143' TERM
 
-download_repository $1
+download_repository "$1"
 dumb-init learn-ocaml --sync=sync --repo=repository &
 PID=$!
 wait $PID
